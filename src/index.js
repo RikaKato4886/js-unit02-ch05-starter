aea4209a1e4311a2be58aa864d85530666d3b7fa
Rika <rika4886@gmail.com>
2020-11-17 22:08:01 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import Polyglot from 'node-polyglot';
 class TranslationApp {
   constructor() {
     this.polyglot = new Polyglot();
-    this.currentLocale = "jp";
-    // this.inputLocale = localStorage.setItem("currentLocate", this.currentLocale)
-    // this.selectedLocale = localStorage.getItem("currentLocate") || 'ja'
-    // localStorageを使うと、デフォルト言語の指定と、選択された言語の取得ができそうですね
+    this.currentLocale = localStorage.getItem(this.currentLocale || 'ja');
+    // localStorage.getItem()を使用する路線で合ってます。ORの演算子を使って、デフォルトで使用できる言語の値と、その時に取得できる言語の値をここで初期値として指定しておくと使いやすそ
   }
 
   setup() {
@@ -22,37 +20,38 @@ class TranslationApp {
     }
   }
 
-  // updateLocale(e) {
-  //   //ボタンにセットされたdata-localeを元に現在のlocaleを変更します。
-  //   console.log(e.target.dataset.locale)
-  //   if (e.target.dataset.locale === "ja"){//日本語ボタンが押された
-  //     this.currentLocate = "ja"
-  //     console.log(`日本語ボタン: currentLocate: ${this.currentLocate}`)
-  //   } else {
-  //     this.currentLocate = "en";
-  //     console.log(`英語ボタン: currentLocate: ${this.currentLocate}`)
-  //   }
-  //   console.log(this.currentLocate)
-  // }
+  updateLocale(e) {
+    //ボタンにセットされたdata-localeを元に現在のlocaleを変更します。
+    if (e.target.dataset.locale === "ja"){//日本語ボタンが押された
+      this.currentLocale = "ja";
+      localStorage.setItem("locale", this.currentLocale)
+      // console.log(this.currentLocale)
+      // this.currentLocate = "ja"
+    } else {
+      this.currentLocale = "en";
+      localStorage.setItem("locale", this.currentLocale)
+      // console.log(this.currentLocale)
+    }
+  }
 
   showMessage() {
     //mainというidがセットされた要素の下にh1タグで現在のlocaleに応じて、メッセージを表示します。
     const mainEl = document.getElementById('main')
-    let h1 = document.createElement('h1');
-    this.setup();
-    h1.innerHTML = this.polyglot.t("message");
-
-    mainEl.appendChild(h1);
+    mainEl.innerHTML = `
+    <h1>
+      ${this.polyglot.t('message')}
+    <h1>
+    `
   }
 }
 
 let app = new TranslationApp();
-app.showMessage();
+app.setup()
 
-// {
-//   const button1 = document.getElementById('button1');
-//   button1.addEventListener("click", app.updateLocale);
+{
+  const button1 = document.getElementById('button1');
+  button1.addEventListener("click", app.updateLocale);
 
-//   const button2 = document.getElementById('button2');
-//   button2.addEventListener("click", app.updateLocale);
-// }
+  const button2 = document.getElementById('button2');
+  button2.addEventListener("click", app.updateLocale);
+}
